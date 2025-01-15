036b7e648973d73cb1c07d855f308091a1f30fbe
yusuke <byysowbach21@gmail.com>
2025-01-15 22:03:19 +0900

    2025/1/15

---
+++ No.244.py
@@ -0,0 +1,7 @@
+# yukicoder_no.244
+
+N = int(input())
+print(N - 1)
+
+# 一行で
+print(int(input()) - 1)
---
+++ No.272.py
@@ -0,0 +1,8 @@
+# yukicoder_no.272
+
+X = int(input())
+
+if X == 0:
+    print(1)
+elif X == 1:
+    print(0)
