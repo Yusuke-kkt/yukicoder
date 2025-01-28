125741f44f994b6b4e93c5a19fedc7bf33ca2490
yusuke <byysowbach21@gmail.com>
2025-01-28 23:22:19 +0900

    2025/1/28

---
+++ No.536.py
@@ -0,0 +1,20 @@
+# yukicoder_no.536
+
+ID = str(input())
+
+if ID[-2:] == "ai":
+    print(ID[0:-2], end = "")
+    print(ID[-2:].upper())
+else:
+    print(ID + "-AI")
+
+
+# 別解
+ID = input()
+if ID[-2:] == "ai":
+    print(ID[:-2]+"AI")
+else:
+    print(ID+"-AI")
+
+# 別解のほうが簡潔
+# 文字列の末尾を大文字に変換するという思考にとらわれず、末尾の2文字までを出力し、そこに "AI" を加えている。
